4514e30d650b18d69dad61a4de875e14df90af9b
Sagar Pednekar <sagarpednekar04@gmail.com>
2025-01-29 01:42:00 +0530

    feat: add useOnline hook to monitor network status and alert user when offline

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import useOnline from "./hooks/useOnline";
 import useSWR from "./hooks/useSWR";
 
 type Todo = {
@@ -8,6 +10,13 @@ type Todo = {
 function App() {
   const url = "https://jsonplaceholder.typicode.com/posts";
   const { data: todos, isLoading, error } = useSWR(url, true);
+
+  const isOnline = useOnline();
+
+  useEffect(() => {
+    if (!isOnline) window.alert("You are offline !!");
+  }, [isOnline]);
+
   if (error) {
     return <div>{error}</div>;
   }
